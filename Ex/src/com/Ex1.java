1c9686dd46134176a7a9b970bdc786da99621697
Nagabhushanam <nagabhushanamn@gmail.com>
2020-09-02 16:16:35 +0530

    Ex

---
+++ Ex/src/com/Ex1.java
@@ -0,0 +1,29 @@
+package com;
+
+public class Ex1 {
+	
+	public static void main(String[] args) {
+		
+		
+		/*
+		 * 
+		 *  inp : .csv
+		 * 
+		 *  op  :
+		 *  
+		 *  	DepName : TotalSalary
+		 *  
+		 *  
+		 *  e.g   IT    : 400
+		 *        HR    : 200
+		 *        MKT   : 400 
+		 * 
+		 */
+		
+		//
+		
+		
+		
+	}
+
+}
